9dd5b6e13a4a8d5e5fd016b94e34c3f1c9c652f2
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-05-02 16:03:03 +1000

    assignment submitted

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        a.length <=> b.length 
+end
+
+def swap_elements(array) 
+    new_array = array.clone  
+    new_array[1] = array[2]
+    new_array[2] = array[1]
+    new_array
+end
+
+def reverse_array(array)
+    array.reverse 
+end
+
+def kesha_maker(array)
+    array.each do |item|
+        item[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select{|i| i[0]=="a"}
+end
+
+def sum_array(array)
+    sum = 0
+    array.each do |i|
+        sum +=i 
+    end 
+    sum
+end
+
+def add_s(array)
+    array.map do |word|
+        if array[1] == word
+            word
+        else
+            word+"s"
+        end
+    end
+end
 
